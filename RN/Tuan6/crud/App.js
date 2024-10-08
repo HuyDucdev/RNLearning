227e413a2610eb9b481e510ba51104155ca14dc7
Huy Duc <dhuy20082991@gmail.com>
2024-10-08 22:44:37 +0700

    Tuan6 - ReactNative - Lab7_a - CRUD - feat: Delete

--- RN/Tuan6/crud/App.js
+++ RN/Tuan6/crud/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef } from "react";
 import {
   View,
   FlatList,
@@ -6,39 +6,23 @@ import {
   Pressable,
   TextInput,
   Modal,
-  ScrollView
-} from 'react-native';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import axios from 'axios';
-import { v4 as uuidv4 } from 'uuid';
-
-const renderItem = ({item}) => (
-<View style = {{flex: 1, alignItems: 'center'}}>
-    
-    <View style = {{flexDirection: 'row', width: 250, backgroundColor: '#D3D5D9', marginVertical: 10, paddingVertical: 10, borderRadius: 20, justifyContent: 'space-around', alignItems: 'center' }}>
-      <Text style = {{fontSize: 18, fontWeight: 700}}>{item.content}</Text>
-      <View style = {{flexDirection: 'row', gap: 5}}>
-        <Pressable >
-          <Ionicons name="create-outline" size={20} color="black" />
-        </Pressable>
-        <Pressable>
-          <Ionicons name="close-outline" size={20} color="black" />
-        </Pressable>
-      </View>
-    </View>
-</View>
-);
+  ScrollView,
+} from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 
 const App = () => {
-
   const [data, setData] = useState([]);
   const [textData, setText] = useState();
   const [modalVisible, setModalVisible] = useState(false);
-  const [responseMessage, setResponseMessage] = useState('');
+  const [responseMessage, setResponseMessage] = useState("");
 
-  const bgRef = useRef("null")
+  const bgRef = useRef("null");
+  const itemRef = useRef();
+
   useEffect(() => {
-    fetch('https://661389d053b0d5d80f6799e5.mockapi.io/test') 
+    fetch("https://661389d053b0d5d80f6799e5.mockapi.io/test")
       .then((response) => response.json())
       .then((json) => {
         setData(json);
@@ -46,10 +30,42 @@ const App = () => {
       })
       .catch((error) => {
         console.error(error);
-        setLoading(false)
+        setLoading(false);
       });
   }, [data]);
 
+  const renderItem = ({ item }) => (
+    <View key={item.id}>
+      <View
+        style={{
+          flexDirection: "row",
+          width: 250,
+          backgroundColor: "#D3D5D9",
+          marginVertical: 10,
+          paddingVertical: 10,
+          borderRadius: 20,
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ fontSize: 18, fontWeight: 700 }}>{item.id}</Text>
+
+        <Text style={{ fontSize: 18, fontWeight: 700 }}>{item.content}</Text>
+        <View style={{ flexDirection: "row", gap: 5 }}>
+          <Pressable>
+            <Ionicons name="create-outline" size={20} color="black" />
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              deleteData(item.id);
+            }}
+          >
+            <Ionicons name="close-outline" size={20} color="black" />
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
 
   const createNewData = async () => {
     const newId = uuidv4(); // Tạo ID mới
@@ -59,23 +75,39 @@ const App = () => {
     };
 
     try {
-      const response = await axios.post('https://661389d053b0d5d80f6799e5.mockapi.io/test', newData);
-      setResponseMessage('Data created successfully!');
+      const response = await axios.post(
+        "https://661389d053b0d5d80f6799e5.mockapi.io/test",
+        newData
+      );
+      setResponseMessage("Data created successfully!");
     } catch (error) {
       if (error.response) {
         setResponseMessage(`Error: ${error.response.data.message}`);
       } else {
-        setResponseMessage('Failed to send data');
+        setResponseMessage("Failed to send data");
       }
     }
   };
 
-
- 
+  const deleteData = async (idDelete) => {
+    try {
+      const response = await axios.delete(
+        `https://661389d053b0d5d80f6799e5.mockapi.io/test/${idDelete}`
+      );
+      setResponseMessage(`Data deleted successfully!`);
+      setData((prevData) => prevData.filter((item) => item.id !== idDelete));
+    } catch (error) {
+      if (error.response) {
+        setResponseMessage(`Error: ${error.response.data.message}`);
+      } else {
+        setResponseMessage("Failed to delete data");
+      }
+    }
+  };
 
   return (
-    <ScrollView style = {{flex: 1, alignItems: 'center'}}  ref = {bgRef}>
-      
+    <ScrollView style={{ flex: 1, alignItems: "center" }} ref={bgRef}>
+      <Text style={{ color: "black" }}>{responseMessage}</Text>
       <Modal
         animationType="fade"
         transparent={true}
@@ -84,51 +116,60 @@ const App = () => {
           setModalVisible(false);
         }}
       >
-        <View style={{flex: 1, alignItems: "center", justifyContent: 'center'}}>
-          <View style={{ width: 200, height: 110, borderWidth: 1, backgroundColor: "black"}}>
-            <Pressable style = {{alignSelf: "flex-end"}} onPress={() => {
-              setModalVisible(false)
-              bgRef.current.style.opacity = "1"}}> 
-              <Ionicons name="close-outline" size={30} color="white" />  
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <View
+            style={{
+              width: 200,
+              height: 110,
+              borderWidth: 1,
+              backgroundColor: "black",
+            }}
+          >
+            <Pressable
+              style={{ alignSelf: "flex-end" }}
+              onPress={() => {
+                setModalVisible(false);
+                bgRef.current.style.opacity = "1";
+              }}
+            >
+              <Ionicons name="close-outline" size={30} color="white" />
             </Pressable>
-            <TextInput style = {{color: "white"}}
-              value = {textData}
-              onChangeText = {setText}
-              placeholder = "Nhap noi dung moi"
+            <TextInput
+              style={{ color: "white" }}
+              value={textData}
+              onChangeText={setText}
+              placeholder="Nhap noi dung moi"
             />
-            <Pressable style = {{alignSelf: "flex-end"}} onPress={createNewData}> 
-              <Ionicons name="checkmark-outline" size={30} color="white" />  
+            <Pressable
+              style={{ alignSelf: "flex-end" }}
+              onPress={createNewData}
+            >
+              <Ionicons name="checkmark-outline" size={30} color="white" />
             </Pressable>
-            <Text style = {{color: "white"}}>{responseMessage}</Text>
-            
           </View>
         </View>
       </Modal>
       <View>
-       <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
         />
       </View>
 
-
-      <Pressable 
-      style = {{left: 100}}
-        onPress = {()=>{
-          setModalVisible(true) 
-          bgRef.current.style.opacity = "0.5"
-        }}>
-          <Ionicons name="add-circle-outline" size={50} color="black" />
+      <Pressable
+        style={{ left: 100 }}
+        onPress={() => {
+          setModalVisible(true);
+          bgRef.current.style.opacity = "0.5";
+        }}
+      >
+        <Ionicons name="add-circle-outline" size={50} color="black" />
       </Pressable>
     </ScrollView>
-
-
- 
-
   );
 };
-
-
 
 export default App;
